f592556f5e43356a7501f43cefe6af79710bcd68
falvey20 <69197089+falvey20@users.noreply.github.com>
2021-05-03 17:06:36 +0100

    Create main.py

---
+++ 022 - Pong/main.py
@@ -0,0 +1,52 @@
+from turtle import Screen, Turtle
+from ball import Ball
+from paddle import Paddle
+import time
+from scoreboard import Scoreboard
+
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Pong")
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+scoreboard = Scoreboard()
+
+
+screen.listen()
+screen.onkey(r_paddle.up, "Up")
+screen.onkey(r_paddle.down, "Down")
+screen.onkey(l_paddle.up, "w")
+screen.onkey(l_paddle.down, "s")
+
+
+game_over = False
+while not game_over:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+
+    # Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # Detect collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    # Detect if R paddle misses
+    if ball.xcor() > 380:
+        scoreboard.l_point()
+        ball.reset_position()
+
+    # Detect if L paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
+
+        
+screen.exitonclick()
